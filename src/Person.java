13677c36b823be5d8ad1e191b7c4ea719fe29da3
Kobe-D <kobejdonovan@gmail.com>
2024-11-30 17:01:58 +1000

    -
    added getters and set paremiters for constructor

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,40 @@
 public class Person {
+//Add at least 3 instance variables suitable for a person
+    private String name;
+    private String contactdetails;
+    private String gender; 
+
+// constructor
+    public Persons(){
+        this.name = "ENTER NAME";
+        this.contactdetails = "ENTER CONTACT";
+        this.gender = "enter gender";
     
+    }
+// making parameter fr constructor 
+
+    public Person(String name, String contactdetails, String gender) {
+        this.name = name;
+        this.contactdetails = contactdetails;
+        this.gender = gender;
+    }
+// setting up the getters for variables 
+
+    public String getName(){
+        return name;
+    }
+
+    public String getContactdetails(){
+        return contactdetails;
+    }
+
+    public String getGender(){
+        return gender;
+    }
+
+
+
+
+
+
 }
