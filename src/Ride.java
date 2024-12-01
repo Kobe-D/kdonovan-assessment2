d95105ca839ce6a85bf2ff53f9eb6c26ffe7521f
Kobe-D <kobejdonovan@gmail.com>
2024-12-01 21:00:06 +1000

    setting up the ride class

--- src/Ride.java
+++ src/Ride.java
@@ -1,5 +1,25 @@
 //The Ride class is used to track the rides available at the theme park, e.g., roller coaster, water riders, etc.
-
 public class Ride {
+    private String rideName;
+    private Employee operator;
+    private boolean isOpen;
+    private int length;
+//constructor   
+public Ride(){
+    this.rideName = "ride name";
+    this.operator = null; 
+    this.isOpen = false;
+    this.length = 0;
+}
+//setting parameters 
+public Ride(String rideName, Employee operator, boolean isOpen, int length){
+    this.rideName = rideName;
+    this.operator = operator;
+    this.isOpen = isOpen;
+    this.length = length;
+}
+//getters amd setters 
+
+
     
 }
