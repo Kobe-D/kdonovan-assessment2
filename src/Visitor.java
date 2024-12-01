7840873dfb9042cf246d39bdf3ba9ebc88eb7d3f
Kobe-D <kobejdonovan@gmail.com>
2024-12-01 17:40:39 +1000

    setting up visator file

--- src/Visitor.java
+++ src/Visitor.java
@@ -1,6 +1,41 @@
 //The Visitor class is used to track the theme park visitors
 //extends the person class
+public class Visitor extends Person{
+//Add at least 2 instance variables suitable for a theme park member 
+    private boolean familyGroup;
+    private boolean adult; 
+    private String  ticketnumber;
+    private boolean student; 
+// constructor 
+public Visitor(){
+    super(); 
+    this.familyGroup = false;
+    this.adult = false;
+    this.ticketnumber = "ticketnumber";
+    this.student = false;
+}
+//setting parematers 
+public Visitor(String name, String mobile, String gender, boolean familyGroup, boolean adult, boolean student, String ticketnumber){
+    super(name, mobile, gender);
+    this.familyGroup = familyGroup;
+    this.adult = adult;
+    this.ticketnumber = ticketnumber;
+    this.student = student;
 
-public class Visitor {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 }
