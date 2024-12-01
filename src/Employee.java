e0ed58a24d5ba2c7c1e91a604a15fe43d5463638
Kobe-D <kobejdonovan@gmail.com>
2024-12-01 17:05:22 +1000

    fixed code issue and continuity issue
    added override display info class  only for employee  so that the  public class Employee extends Person dosnt have a red underline.
    
    fixed continuity with Employee:  and  Employee Info  to being both Employee Info in the overide's

--- src/Employee.java
+++ src/Employee.java
@@ -19,7 +19,6 @@ public Employee(String name, String mobile, String gender, String role, String u
 
 }
 // implementing getters and setter 
-
 public String getRole() {
     return role; 
 }
@@ -35,14 +34,19 @@ public void setRole(String role) {
 public void setUID(String uID) {
     this.uID = uID;
 }
-
 //overide method so the employee class can communicate to Person class 
 //this was used in the last assessment under the recomendation of gen AI
 //so i will still refer to the @overide code being Ai generated. 
 @Override
+public void displayInfo() {
+    // add a method to display the employee info
+    System.out.println("Employee Info: " + getName() + ", " + getRole() + ", " + getUID());
+}
+
+@Override
 
 public String toString() {  //to string allows a better formatted employee output
-    return "Employee: " + uID + ", " + getName() + ", " + getMobile() + ", " + getGender() + ", " + role;
+    return "Employee Info: " + uID + ", " + getName() + ", " + getMobile() + ", " + getGender() + ", " + role;
 }
 
 }
