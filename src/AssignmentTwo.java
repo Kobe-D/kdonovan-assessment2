038cda312b99500f62768ed96c24d4dddde2ca59
Kobe-D <kobejdonovan@gmail.com>
2024-12-04 00:26:18 +1000

    Started part3 output method
    added new ride
    
    added new visitor

--- src/AssignmentTwo.java
+++ src/AssignmentTwo.java
@@ -3,7 +3,20 @@ public class AssignmentTwo {
     public static void main(String[] args) {
     }
     public void partThree(){
+    //making new ride
+    Ride ride1 = new Ride("Ride 1", null, true, 3);
+
+// creating a list of visitors 
+                        //name  number   generder
+    Visitor v001 = Visitor("Tame Impala", "0444444444", "Male", false, true, false, "T0001");
+    Visitor v002 = Visitor("Kurt Cobain", "0477777777", "Male", true, true, false, "T0002");
+
+
+
+
+
     }
+
     public void partFourA(){
     }
     public void partFourB(){
