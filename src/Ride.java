57b69764d706d708cf6f89c1669e5aaa995a6f0f
Kobe-D <kobejdonovan@gmail.com>
2024-12-04 04:01:21 +1000

    moving code around to try and get the method to print as its been failing

--- src/Ride.java
+++ src/Ride.java
@@ -120,6 +120,51 @@ public void printQueue(){
         }
     }
 }
+// TESTING PART 3 |:
+   //making new ride
+   public void partThree() {
+   
+   // creating a list of visitors                         
+       Visitor v001 = new Visitor("Tame Impala", "0444444444", "Male", false, true, false, "T0001");
+       Visitor v002 = new Visitor("Kurt Cobain", "0477777777", "Male", true, true, false, "T0002");
+       Visitor v003 = new Visitor("Jordan Carter", "066666666", "Male", false, true, true, "T0003");
+       Visitor v004 = new Visitor("Pastel Ghost", "0202020202", "Female", false, true, false, "T0004");
+       Visitor v005 = new Visitor("Pixies", "4545454544", "Male", true, true, false, "T0005");
+       Visitor v006 = new Visitor("Tony Hawk", "46565456163", "Male", false, false, true, "T0006");
+       Visitor v007 = new Visitor("Eric Smith", "741852963", "Male", false, true, true, "T0007");
+       Visitor v008 = new Visitor("Jotaro Kujo", "4785245654", "Male", false, false, true, "T0008");
+       Visitor v009 = new Visitor("Jessica Rabbit", "098527368", "Female", false, false, false, "T0009");
+       Visitor v010 = new Visitor("Jade Pirf", "416468314", "Female", true, true, true, "T0010");
+       
+   // putting visitors in the queue 
+       ride1.addVisitortoQueue(v010);
+       ride1.addVisitortoQueue(v009);
+       ride1.addVisitortoQueue(v008);
+       ride1.addVisitortoQueue(v007);
+       ride1.addVisitortoQueue(v006);
+       ride1.addVisitortoQueue(v005);
+       ride1.addVisitortoQueue(v004);
+       ride1.addVisitortoQueue(v003);
+       ride1.addVisitortoQueue(v002);
+       ride1.addVisitortoQueue(v001);
+   //removing from queue
+       ride1.removeVisitorfromQueue(v005);
+       ride1.removeVisitorfromQueue(v008);
+       ride1.removeVisitorfromQueue(v001);
+       
+   // pring method 
+       ride1.printQueue();
+
+   }
+
+
+
+
+
+
+
+
+
 @Override
 public void runOneCycle() {
 // part 5 implemnation
