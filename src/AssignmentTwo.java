f2be439e22975001277713a5441f0e5fa0cf197f
Kobe-D <kobejdonovan@gmail.com>
2024-12-04 00:39:38 +1000

    Added 10 new visitors

--- src/AssignmentTwo.java
+++ src/AssignmentTwo.java
@@ -10,7 +10,15 @@ public class AssignmentTwo {
                         //name  number   generder
     Visitor v001 = new Visitor("Tame Impala", "0444444444", "Male", false, true, false, "T0001");
     Visitor v002 = new Visitor("Kurt Cobain", "0477777777", "Male", true, true, false, "T0002");
-
+    Visitor v003 = new Visitor("Jordan Carter", "066666666", "Male", false, true, true, "T0003");
+    Visitor v004 = new Visitor("Pastel Ghost", "0202020202", "Female", false, true, false, "T0004");
+    Visitor v005 = new Visitor("Pixies", "4545454544", "Male", true, true, false, "T0005");
+    Visitor v006 = new Visitor("Tony Hawk", "46565456163", "Male", false, false, true, "T0006");
+    Visitor v007 = new Visitor("Eric Smith", "741852963", "Male", false, true, true, "T0007");
+    Visitor v008 = new Visitor("Jotaro Kujo", "4785245654", "Male", false, false, true, "T0008");
+    Visitor v009 = new Visitor("Jessica Rabbit", "098527368", "Female", false, false, false, "T0009");
+    Visitor v010 = new Visitor("Jade Pirf", "416468314", "Female", true, true, true, "T0010");
+    
 
 
 
